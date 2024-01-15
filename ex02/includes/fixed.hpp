114df05bd95c88a3aa3f5bf59c2692e4b8502bc5
Ahlem NOURI GALLALI <114947953+AhlemNG@users.noreply.github.com>
2024-01-15 07:44:43 +0000

    operators

---
+++ ex02/includes/fixed.hpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anouri <anouri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 10:35:00 by anouri            #+#    #+#             */
+/*   Updated: 2024/01/12 18:16:40 by anouri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Orthodox canonical form:
+    the calss must contain at least:
+        - 1 default constructor
+        - 2 copy constructor
+        - 3 copy assignment operator
+        - 4 destructor
+*/
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+class Fixed
+{
+    private:
+        /* data */
+        int _value;
+        static  int const _nbbits = 8;
+    public:
+        Fixed(/* args */); // 1 deFault cosntructor
+        Fixed(Fixed const & src); // 2 copy constructor cosntructor
+        Fixed(int const value); // integer constructor
+        Fixed(float const value); // float constructor
+        Fixed & operator=(Fixed const & rhs); // 3 copy asssgniment operator
+        /*comparaison operators*/
+        // Fixed & operator>(Fixed const & rhs); 
+        // Fixed & operator<(Fixed const & rhs);
+        // Fixed & operator>=(Fixed const & rhs);
+        // Fixed & operator<=(Fixed const & rhs);
+        // Fixed & operator==(Fixed const & rhs);
+        // Fixed & operator!=(Fixed const & rhs);
+        /*arithmetic operators*/
+        Fixed  operator+(Fixed const & rhs) const; 
+        Fixed  operator-(Fixed const & rhs) const;
+        Fixed  operator*(Fixed const & rhs) const;
+        Fixed  operator/(Fixed const & rhs) const;
+        /*pre-incremetation operators*/
+        Fixed & operator++(int a); 
+        // /*post-incremetation operators*/
+        // Fixed & operator++(Fixed const & rhs); 
+        // /*pre-decremetation operators*/
+        // Fixed & operator--(Fixed const & rhs); 
+        // /*post-decremetation operators*/
+        // Fixed & operator--(Fixed const & rhs); 
+        ~Fixed(); // 4 destrutor
+        int getRawBits(void) const;
+        void setRawBits( int const raw );
+        float toFloat(void) const;
+        int toInt(void) const;
+        /*an overload of the insetion (<<) operator*/
+    };
+
+    std::ostream &operator<<(std::ostream& o, Fixed const& rhs);
+
+/*An overload of the insertion («) operator that inserts a floating-point representation
+of the fixed-point number into the output stream object passed as parameter.*/
+
+#endif
---
+++ ex02/srcs/main.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anouri <anouri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 10:10:32 by anouri            #+#    #+#             */
+/*   Updated: 2024/01/12 18:13:18 by anouri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fixed.hpp"
+#include <iostream>
+
+int main()
+{
+    Fixed a;
+    Fixed const b(10);
+    Fixed const c(42.42f );
+
+    Fixed const d(b);
+
+    a = Fixed(1234.421f);
+    
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
+    std::cout << "a is " << a.toInt() << " as an integer" <<std::endl;
+    std::cout << "b is " << b.toInt() << " as an integer" <<std::endl;
+    std::cout << "c is " << c.toInt() << " as an integer" <<std::endl;
+    std::cout << "d is " << d.toInt() << " as an integer" <<std::endl;
+
+    std::cout << "b + d is " << (b + d).toInt() << " as an integer" <<std::endl;
+ 
+    return 0;
+}
