96a696f512b143debfcfe8f6afa9e07163db98eb
Ahlem NOURI <anouri@student.42.fr>
2024-01-19 15:12:49 +0100

    ex03

--- ex03/srcs/bsp.cpp
+++ ex03/srcs/bsp.cpp
@@ -6,12 +6,12 @@
 /*   By: anouri <anouri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 16:22:36 by anouri            #+#    #+#             */
-/*   Updated: 2024/01/16 10:11:01 by anouri           ###   ########.fr       */
+/*   Updated: 2024/01/19 15:08:52 by anouri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Point.hpp"
-
+#include <iostream>
 /*returns true if point is in triangle abc othewise returns false*/
 
 bool bsp( Point const a, Point const b, Point const c, Point const point)
@@ -19,10 +19,11 @@ bool bsp( Point const a, Point const b, Point const c, Point const point)
     Fixed W1(2);
     Fixed W2(1);
  
-    W1 = (a.getX()*(c.getY()-a.getY()) + (point.getY()-a.getY())*(c.getX()-a.getX()) - point.getX()*(c.getY()-a.getY())) /
-        ((b.getY()-a.getY())*((c.getX()- (a.getX()))-(b.getX()-a.getX())*(c.getY()-a.getY())));
+    std::cout << "a.x is " << a.getX() << std::endl;
     
-    W2 = point.getY() - a.getY() - W1 *(b.getY()-a.getY()) /
-        (c.getY() - a.getY());
+    W1 = (a.getX()*(c.getY()-a.getY()) + (point.getY()-a.getY())*(c.getX()-a.getX()) - point.getX()*(c.getY()-a.getY())) /((b.getY()-a.getY())*((c.getX()- (a.getX()))-(b.getX()-a.getX())*(c.getY()-a.getY())));
+    std::cout << "W1 is " << W1 << std::endl;
+    W2 = point.getY() - a.getY() - W1 *(b.getY()-a.getY()) / (c.getY() - a.getY());
+    std::cout << "W2 is " << W2 << std::endl; 
     return ((W1>= 0 && W2 >= 0 && W1+W2 <= 1) ? true : false);
 }
--- ex03/srcs/main.cpp
+++ ex03/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: anouri <anouri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 10:10:32 by anouri            #+#    #+#             */
-/*   Updated: 2024/01/16 10:27:45 by anouri           ###   ########.fr       */
+/*   Updated: 2024/01/19 15:04:28 by anouri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@
 int main(void)
 {
     Point const a(10, 10);
-    Point const b(15, 20);
-    Point const c(7, 5);
-    Point const d(8, 11);
+    Point const b(3, 15);
+    Point const c(2, 6);
+    // Point const d(8, 11);
+    Point const d(5,10);
     
     std::cout << std::boolalpha << bsp(a, b, c, d) <<std::endl;
     return 0;
